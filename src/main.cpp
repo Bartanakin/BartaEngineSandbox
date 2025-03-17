6bc218fe309525c384c2635ce7237098026efc6e
bartanakin <bartanakin@interia.pl>
2025-03-17 19:47:59 +0100

    #42

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
-//
-// Created by barta on 01/03/2024.
-//
-
 #include "Sandbox.h"
-#include "Dynamics/Timers/FrameLimitTimerProxy.h"
-#include "Dynamics/Timers/SFML_Timer.h"
 
 int main() {
-	auto app = new Sandbox();
-	app->run();
-	delete app;
+    auto app = new Sandbox();
+    app->run();
+    delete app;
 
     return 0;
 }
